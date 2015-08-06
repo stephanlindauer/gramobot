142d60fab57b2291eceb7cc0cdecb07813e41101
stephan lindauer <stephan.lindauer@gmail.com>
2015-08-06 22:07:08 +0200

    test docker integration

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-rm -r public
-rm -r bower_components
-rm -r node_modules
-npm ls -g | grep brunch || npm install -g brunch
-npm ls -g | grep bower || npm install -g bower
+npm install -g brunch
+npm install -g bower
 npm install
 bower install
-brunch b
-brunch watch
+brunch build --production
+npm start
